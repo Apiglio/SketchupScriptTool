aba8770b4d0a07c87ed77e5f35ea5e93b21f1e8d
Apiglio <553724286@qq.com>
2024-12-22 21:58:32 +0800

    APUI::EntityFilter

--- apui.rb
+++ apui.rb
@@ -1,3 +1,6 @@
+#encoding "UTF-8"
+require __dir__+'/class/instpath_helper.rb'
+
 module APUI
 	#存储选项的inputbox
 	class StoredInputBox
@@ -17,4 +20,100 @@ module APUI
 		end
 	end
 	
+	module EntityFilter
+		@type_caption_map = {"Text":"文本标注","DimensionLinear":"尺寸","DimensionRadial":"直径标注"}
+		def self.search_for_entities(typelist, orientlist, min_length, max_length)
+			list = InstancePathTree.check_subordinate(nil).subordinates
+			list.select!{|p|typelist.include?(p.leaf.class)}
+			result_list = [];
+			list.each{|path|
+				case path.leaf
+					when Sketchup::Text
+						vec = path.leaf.vector ? path.transformation*path.leaf.vector : Geom::Vector3d.new([0,0,0])
+					when Sketchup::DimensionLinear
+						vec = path.transformation*path.leaf.offset_vector
+					when Sketchup::DimensionRadial
+						pts = path.leaf.leader_points
+						vec = pts[2] - pts[1]
+						vec = path.transformation*vec
+					else
+						raise TypeError.new("Unexpected type of InstancePath leaf Entity #{path.leaf}.")
+				end
+				len = vec.length
+				if len>0 then
+					next unless len>=min_length and len<=max_length
+					# Elsif地狱，可以修改逻辑
+					if vec.parallel?([0,0,1]) then
+						next unless orientlist.include?('z')
+					elsif vec.parallel?([0,1,0]) then
+						next unless orientlist.include?('y')
+					elsif vec.parallel?([1,0,0]) then
+						next unless orientlist.include?('x')
+					elsif vec.perpendicular?([0,0,1]) then
+						next unless orientlist.include?('xy')
+					elsif vec.perpendicular?([0,1,0]) then
+						next unless orientlist.include?('xz')
+					elsif vec.perpendicular?([1,0,0]) then
+						next unless orientlist.include?('yz')
+					else
+						next unless orientlist.include?('xyz')
+					end
+				else
+					next unless orientlist.include?('0')
+				end
+				result_list << path.leaf
+			}
+			return result_list
+		end
+		def self.showUI()
+			unless defined?(@window) then
+				@window = UI::HtmlDialog.new(
+				{
+				  :dialog_title => "Apiglio Entity Filter",
+				  :preferences_key => "- Apiglio -",
+				  :scrollable => true,
+				  :resizable => true,
+				  :width => 300,
+				  :height => 600,
+				  :min_width => 300,
+				  :min_height => 600,
+				  :style => UI::HtmlDialog::STYLE_UTILITY
+				})
+				@window.set_file(__dir__+"/UI/EntityFilter.html")
+			end
+			
+			@window.add_action_callback("do_filter"){
+				|action_context, typelist, orientlist, e1, e2, u1, u2|
+				typelist.map!{|t|eval("Sketchup::"+t)}
+				min_length = eval(e1.to_s+"."+u1)
+				max_length = eval(e2.to_s+"."+u2)
+				res = search_for_entities(typelist, orientlist, min_length, max_length)
+				res.each{|ent|
+					@window.execute_script("appendItem(\"#{("      "+ent.typename)[-6..-1]}\", \"#{ent.text}\", #{ent.persistent_id})")
+				}
+			}
+			@window.add_action_callback("do_zoom"){
+				|action_context, pid|
+				ent = Sketchup.active_model.find_entity_by_persistent_id(pid)
+				Sketchup.active_model.active_view.zoom(ent)
+				Sketchup.active_model.selection.clear
+				Sketchup.active_model.selection.add(ent)
+			}
+			@window.show
+		end
+		unless defined?(@command) then
+			@command = UI::Command.new("Open Filter Window") {
+				self.showUI
+			}
+			@command.menu_text = "打开筛选器窗口"
+			@command.tooltip = "打开筛选器窗口"
+			@command.status_bar_text = "打开 Apiglio Entity Filter 的筛选器窗口"
+			@command.small_icon = __dir__+"/UI/EntityFilter_S.png"
+			@command.large_icon = __dir__+"/UI/EntityFilter_L.png"
+			@command.set_validation_proc { MF_ENABLED }
+			@toolbar = UI::Toolbar.new("Apiglio Entity Filter")
+			@toolbar = @toolbar.add_item(@command)
+			@toolbar.show
+		end
+	end
 end
